2508b039ec1a0426fe339709d2ddc28c604bd6cd
Josipa <josipa0826@gmail.com>
2024-10-17 16:21:11 +0200

    commit ignore

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/josip/Documents/dev/_cloned%20website/PortfolioJosipa/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('C:/Users/josip/Documents/dev/_cloned website/PortfolioJosipa/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('C:/Users/josip/Documents/dev/_cloned website/PortfolioJosipa/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
