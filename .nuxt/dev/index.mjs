36149e75d53f8728585bcfd37c99edd0e9b754de
Josipa <josipa0826@gmail.com>
2024-12-04 19:04:28 +0100

    bg change

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -220,7 +220,7 @@ const _inlineRuntimeConfig = {
         "tr": "prose-tr"
       },
       "highlight": false,
-      "wsUrl": "ws://localhost:4002/",
+      "wsUrl": "ws://localhost:4001/",
       "documentDriven": {
         "page": true,
         "navigation": true,
